c0f38d2575fa74bd15a6709d21418c2eeb52fc49
bushbass <alex.nielsen@gmail.com>
2019-12-27 11:15:27 -0500

    initial commit

---
+++ server.js
@@ -0,0 +1,50 @@
+const express = require('express');
+
+const mongoose = require('mongoose');
+require('dotenv').config({ path: 'variables.env' });
+
+const Recipe = require('./models/Recipe');
+const User = require('./models/User');
+
+// Bring in graphql express middleware
+const { graphiqlExpress, graphqlExpress } = require('apollo-server-express');
+const { makeExecutableSchema } = require('graphql-tools');
+
+const { typeDefs } = require('./schema');
+const { resolvers } = require('./resolvers');
+
+// create schema
+const schema = makeExecutableSchema({
+  typeDefs,
+  resolvers
+});
+
+// Connects to database
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log('DB Connected'))
+  .catch(err => console.log(err));
+
+// Initializes application
+const app = express();
+
+// create graphiql application
+app.use('/graphiql', graphiqlExpress({ endpointURL: './graphql' }));
+
+// connect schemas with graphql
+app.use(
+  '/graphql',
+  graphqlExpress({
+    schema,
+    context: {
+      Recipe,
+      User
+    }
+  })
+);
+
+const PORT = process.env.PORT || 4444;
+
+app.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
+});
