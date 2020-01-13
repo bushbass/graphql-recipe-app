7d4eaea76009cb6d4a546494014b7965fb8f1f85
bushbass <alex.nielsen@gmail.com>
2020-01-13 02:53:17 -0500

    finished lesson 32

--- client/src/index.js
+++ client/src/index.js
@@ -12,6 +12,7 @@ import './index.css';
 import App from './components/App';
 import Signin from './components/Auth/Signin';
 import Signup from './components/Auth/Signup';
+import withSession from './components/withSession';
 
 const client = new ApolloClient({
   uri: 'http://localhost:4444/graphql',
@@ -43,10 +44,11 @@ const Root = () => (
     </Switch>
   </Router>
 );
+const RootWithSession = withSession(Root);
 
 ReactDOM.render(
   <ApolloProvider client={client}>
-    <Root />
+    <RootWithSession />
   </ApolloProvider>,
   document.getElementById('root')
 );
--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 
 const bodyParser = require('body-parser');
 const cors = require('cors');
-
+const jwt = require('jsonwebtoken');
 require('dotenv').config({ path: 'variables.env' });
 
 const Recipe = require('./models/Recipe');
@@ -44,7 +44,14 @@ app.use(cors(corsOptions));
 // Set up JWT authentication middleware
 app.use(async (req, res, next) => {
   const token = req.headers['authorization'];
-  console.log(token);
+  if (token !== 'null') {
+    try {
+      const currentUser = await jwt.verify(token, process.env.SECRET);
+      req.currentUser = currentUser;
+    } catch (error) {
+      console.error(error);
+    }
+  }
   next();
 });
 
@@ -55,13 +62,14 @@ app.use('/graphiql', graphiqlExpress({ endpointURL: './graphql' }));
 app.use(
   '/graphql',
   bodyParser.json(),
-  graphqlExpress({
+  graphqlExpress(({ currentUser }) => ({
     schema,
     context: {
       Recipe,
-      User
+      User,
+      currentUser
     }
-  })
+  }))
 );
 
 const PORT = process.env.PORT || 4444;
